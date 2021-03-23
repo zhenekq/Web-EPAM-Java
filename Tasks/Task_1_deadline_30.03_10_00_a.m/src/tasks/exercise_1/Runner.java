b9a8a490e9954a73d1b2e4e9b3e769d1fa943673
zhenekq <zhenek02ss@gmail.com>
2021-03-23 23:30:06 +0300

    feat: completed 1st exercise

--- Tasks/Task_1_deadline_30.03_10_00_a.m/src/tasks/exercise_1/Runner.java
+++ Tasks/Task_1_deadline_30.03_10_00_a.m/src/tasks/exercise_1/Runner.java
@@ -4,5 +4,9 @@ public class Runner {
     public static void main(String[] args){
         System.out.println("Enter our current number: ");
         int ourCurrentNumber = ReadNumber.readOurCurrentNumber();
+        int lastDigitOfOurCurrentNumber = LogicalFunctions.lastDigitOfOurCurrentNumber(ourCurrentNumber);
+        int squareOfLastDigit = LogicalFunctions.lastDigitOfOurCurrentNumberInSquare(lastDigitOfOurCurrentNumber);
+        int lastDigitOfSquare = LogicalFunctions.lastDigitOfOurCurrentNumber(squareOfLastDigit);
+        System.out.println("Last digit of last digit in square of number is: " + lastDigitOfSquare);
     }
 }
