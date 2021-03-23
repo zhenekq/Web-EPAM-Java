8745c359e5f4bf453e5e9f4e417ec6d792ef5079
zhenekq <zhenek02ss@gmail.com>
2021-03-23 21:44:45 +0300

    feat: created function for scanning integer value from keyboard

---
+++ Tasks/Task_1_deadline_30.03_10_00_a.m/src/tasks/exercise_1/ReadNumber.java
@@ -0,0 +1,17 @@
+package tasks.exercise_1;
+
+import java.util.Scanner;
+
+public class ReadNumber {
+    @SuppressWarnings("resource")
+    public static int readOurCurrentNumber(){
+        Scanner scanner = new Scanner(System.in);
+        int ourCurrentNumber = 0;
+        while(!scanner.hasNextInt()){
+            scanner.nextLine();
+            System.out.println("Please! Enter number with data type -> "+"'int': ");
+        }
+        ourCurrentNumber = scanner.nextInt();
+        return ourCurrentNumber;
+    }
+}
---
+++ Tasks/Task_1_deadline_30.03_10_00_a.m/src/tasks/exercise_1/Runner.java
@@ -0,0 +1,9 @@
+package tasks.exercise_1;
+
+public class Runner {
+    public static void main(String[] args){
+        System.out.println("Enter our current number: ");
+        int ourCurrentNumber = ReadNumber.readOurCurrentNumber();
+
+    }
+}
